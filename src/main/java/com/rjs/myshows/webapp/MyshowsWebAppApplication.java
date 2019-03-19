60355e219f206e9f33b011887dcb5612eb63ab28
Randy Strobel <randy@marxent.com>
2019-03-19 16:28:10 -0400

    Initial code commit.

---
+++ src/main/java/com/rjs/myshows/webapp/MyshowsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.rjs.myshows.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshowsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshowsWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjs/myshows/webapp/MyshowsWebAppApplicationTests.java
@@ -0,0 +1,19 @@
+package com.rjs.myshows.webapp;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyshowsWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
